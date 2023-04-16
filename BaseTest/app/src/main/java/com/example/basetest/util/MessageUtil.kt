cb3a08f6f5c121117b3c787ff64056573f4a8f89
wjddnwls918 <wjddnwls918@naver.com>
2023-04-16 17:25:37 +0900

    모듈 주석 추가

--- BaseTest/app/src/main/java/com/example/basetest/util/MessageUtil.kt
+++ BaseTest/app/src/main/java/com/example/basetest/util/MessageUtil.kt
@@ -3,6 +3,7 @@ package com.example.basetest.util
 import android.content.Context
 import android.widget.Toast
 
+// presentation or common layer module
 object MessageUtil {
     fun showDefaultToast(context: Context, message: String) {
         Toast.makeText(context, message, Toast.LENGTH_LONG).show()
